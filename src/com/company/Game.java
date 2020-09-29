b8f53b50e1b53886f7309dd9a6c7cf6f182ace89
professorqtaku <professorqtaku@gmail.com>
2020-09-29 21:09:41 +0200

    added method mainMenuAction in the class Menu

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -9,13 +9,28 @@ public class Game {
 
 
     public Game(){
-        menu = new Menu();
+        menu = new Menu(this);
         gameStart();
     }
 
     public void gameStart(){
         menu.printMainMenu();
+        int userChoice = menu.askPlayer(false, "", "1","2","3");
+        menu.mainMenuAction(userChoice);
+    }
+
+    public void newGame(){
 
     }
+
+    public void loadGame(){
+
+    }
+
+    public void saveGame(){
+
+    }
+
+    public void endGame(){}
 
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -3,8 +3,10 @@ package com.company;
 import java.util.Scanner;
 
 public class Menu {
+    Game game;
     Scanner scanner = new Scanner(System.in);
-    public Menu(){
+    public Menu(Game game){
+        this.game = game;
     }
 
     public void printMainMenu(){
@@ -13,11 +15,24 @@ public class Menu {
         print("1. New game");
         print("2. Load game");
         print("3. End game");
-        askPlayer(false, "", "1","2","3");
+    }
+
+    public void mainMenuAction(int action){
+        switch(action){
+            case 1 ->{
+                game.newGame();
+            }
+            case 2 ->{
+                game.loadGame();
+            }
+            case 3 ->{
+                game.endGame();
+            }
+        }
     }
 
 
-    public void askPlayer(boolean print, String question, String ...choices){
+    public int askPlayer(boolean print, String question, String ...choices){
         if(print){
             System.out.println(question);
             for (int i = 1; i <= choices.length; i++) {
@@ -25,12 +40,19 @@ public class Menu {
             }
         }
         print(TextColour.GREEN + "Your choice: " + TextColour.RESET);
-        try{
-            String userChoice = scanner.next();
-        }
-        catch(Exception e){
-            System.out.println("Error: "e);
-        }
+        int userChoice;
+        do{
+            try {
+                userChoice = Integer.parseInt(scanner.next());
+                if(userChoice > choices.length){
+                    System.out.println("Please enter a number between 1-" + choices.length);
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter an integer!");
+                userChoice = -1;
+            }
+        }while(userChoice == -1 || userChoice > choices.length);
+        return userChoice;
     }
 
     private void print(String x){
